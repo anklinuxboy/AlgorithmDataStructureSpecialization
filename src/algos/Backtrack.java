28d9cd00dabab11cbefc172e7bb67681b06a5492
Ankit Sharma <ankitsharma.nov11@gmail.com>
2019-02-04 07:24:38 -0500

    backtrack permutations

--- src/algos/Backtrack.java
+++ src/algos/Backtrack.java
@@ -2,24 +2,46 @@ package algos;
 
 public class Backtrack {
     public static void main(String[] args) {
-        int[] a = new int[100];
+        int[] a = new int[1000];
 
         Backtrack backtrack = new Backtrack();
         backtrack.backtrack(a, 0, 3);
     }
 
     private void backtrack(int[] a, int k, int input) {
-        int[] c = new int[100];
+        int[] c = new int[1000];
         if (isASolution(a, k, input)) {
-            processSolution(a, k);
+            processSolutionPerm(a, k);
         } else {
             k = k + 1;
-            int candidates = constructCandidates(a, k, input, c);
+            int candidates = constructCandidatesPerm(a, k, input, c);
             for (int i = 0; i < candidates; i++) {
                 a[k] = c[i];
                 backtrack(a, k, input);
             }
         }
+    }
+
+    private int constructCandidatesPerm(int[] a, int k, int input, int[] c) {
+        boolean[] inPerm = new boolean[1000];
+
+        for (int i = 1; i < 1000; i++) {
+            inPerm[i] = false;
+        }
+
+        for (int i = 0; i < k; i++) {
+            inPerm[a[i]] = true;
+        }
+
+        int nCandidates = 0;
+        for (int j = 1; j <= input; j++) {
+            if (!inPerm[j]) {
+                c[nCandidates] = j;
+                nCandidates += 1;
+            }
+        }
+
+        return nCandidates;
     }
 
     private int constructCandidates(int[] a, int k, int input, int[] c) {
@@ -42,5 +64,13 @@ public class Backtrack {
             }
         }
         System.out.println(" }");
+    }
+
+    private void processSolutionPerm(int[] a, int k) {
+        for (int i = 1; i <= k; i++) {
+            System.out.print(" " + a[i]);
+        }
+
+        System.out.println();
     }
 }
